d999e890a108501ac88359a5b95a0c63446ba3a2
Boet Waldbjorn <you@example.com>
2016-07-06 12:31:26 -0700

    added alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,8 @@ var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+if(cardFour === cardThree){
+	alert("You Found A Match!");
+} else {
+	alert("Sorry, Try Again.");
+}
